a2637f4e319af553204763636a6494e53b01282a
juniorjrml <jancarlolcj@gmail.com>
2020-11-16 13:24:02 -0300

    primeiro commit, desenvolvendo o melhor modelo para o deploy

---
+++ tratamento_dados.py
@@ -0,0 +1,64 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import confusion_matrix
+from mlxtend.plotting import plot_confusion_matrix
+from matplotlib import pyplot as plt
+import pandas as pd
+
+
+# normalizando
+def normalizando(entradas):
+  normaliza = MinMaxScaler()  # objeto para a normalização
+  entradas_normalizadas = normaliza.fit_transform(entradas)
+  return entradas_normalizadas
+
+def testando(modelo, X, y):
+  predicao = modelo.predict(X)
+  avaliacao(y,predicao)
+  return modelo.score(X,y)
+
+# Avaliando o modelo
+def avaliacao(y_test, y_pred):
+  matriz_confusao = confusion_matrix(y_test, y_pred)
+  fig, ax = plot_confusion_matrix(conf_mat=matriz_confusao)
+  plt.show()
+
+# separando o dataset em treino e teste e normalizando
+def trata_dataset(X, y):
+  # Normalizando \/
+  entradas_normalizadas = normalizando(X)
+
+  # Dividindo o dataset para treinar e testar
+  X_train, x_test, y_train, y_test = train_test_split(entradas_normalizadas, y, test_size=0.30,random_state=42)
+
+  return X_train, x_test, y_train, y_test
+
+dataset = pd.read_csv("pima-indians-diabetes.csv", header=None)
+X = dataset.drop([8], axis=1) # features
+y = dataset[8] # Alvo
+
+clf_mlp = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5,10), random_state=1)
+clf_arvore = DecisionTreeClassifier(random_state=1)
+clf_KNN = KNeighborsClassifier(n_neighbors=5)
+nome_modelos = ["Rede MLP Classificador", "KNN Classificador", "Arvore Classificador"]
+modelos = [clf_mlp, clf_KNN, clf_arvore]
+
+X_train, x_test, y_train, y_test = trata_dataset(X, y)
+
+for m in modelos:
+  m.fit(X_train, y_train)
+
+for m in enumerate(modelos):
+  print(nome_modelos[m[0]])
+  print("Score = ",testando(m[1], x_test, y_test))
+
+
+
+"""print(dataset.head())
+print(X.head())
+print(y.head())"""
